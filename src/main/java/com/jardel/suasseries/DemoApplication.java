2b059d0c0dd8a56966cd09aac1642486dc457cbb
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-01-17 02:25:05 -0300

    add anotações de mapeamento

--- src/main/java/com/jardel/suasseries/DemoApplication.java
+++ src/main/java/com/jardel/suasseries/DemoApplication.java
@@ -2,8 +2,12 @@ package com.jardel.suasseries;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan(basePackages={"com.jardel.models"})
+@ComponentScan(basePackages={"com.jardel.*"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
