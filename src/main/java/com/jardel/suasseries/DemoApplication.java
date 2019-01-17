404bba33e8e456057349375dc17cc8ef22851e4d
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-01-17 02:32:37 -0300

    add anotação de transação e mapeamento dos repositorys

--- src/main/java/com/jardel/suasseries/DemoApplication.java
+++ src/main/java/com/jardel/suasseries/DemoApplication.java
@@ -1,13 +1,18 @@
 package com.jardel.suasseries;
 
+import javax.transaction.Transactional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages={"com.jardel.models"})
 @ComponentScan(basePackages={"com.jardel.*"})
+@EnableJpaRepositories(basePackages={"com.jardel.repository"})
+@Transactional
 public class DemoApplication {
 
 	public static void main(String[] args) {
