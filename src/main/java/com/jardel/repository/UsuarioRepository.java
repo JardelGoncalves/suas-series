928135e3b033914fcc964ce88bf0910e6235613a
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-01-18 00:22:32 -0300

    add query para consultar usuario por email

--- src/main/java/com/jardel/repository/UsuarioRepository.java
+++ src/main/java/com/jardel/repository/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.jardel.repository;
 
 import com.jardel.models.Usuario;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,5 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @Transactional
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+
+    @Query("select u from Usuario u where u.email = ?1")
+    Usuario findUserByEmail(String email);
 
 }
