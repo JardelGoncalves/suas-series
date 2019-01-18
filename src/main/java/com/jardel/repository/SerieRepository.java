96368e2bccfb6a32449e2f419b3a90669896764a
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-01-18 16:44:31 -0300

    add query para obter as series de um usuario

--- src/main/java/com/jardel/repository/SerieRepository.java
+++ src/main/java/com/jardel/repository/SerieRepository.java
@@ -1,7 +1,10 @@
 package com.jardel.repository;
 
+import java.util.List;
+
 import com.jardel.models.Serie;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,5 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @Transactional
 public interface SerieRepository extends CrudRepository<Serie, Integer>{
+
+    @Query("select s from Serie s where s.usuario.id = ?1")
+    List<Serie> findSerieByUser(int usuario_id);
 
 }
