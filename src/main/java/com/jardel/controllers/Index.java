de94b821e0e3ef9031dcd89303e14be726eb29f4
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-01-18 00:27:18 -0300

    obtendo usuario caso esteja logado e redenrizando corretamente o index.html

--- src/main/java/com/jardel/controllers/Index.java
+++ src/main/java/com/jardel/controllers/Index.java
@@ -1,15 +1,28 @@
 package com.jardel.controllers;
 
+import com.jardel.models.Usuario;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class Index{
+public class Index {
 
     @GetMapping("/")
-    public ModelAndView indexPage(){
+    public ModelAndView indexPage() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Usuario usuario = new Usuario();
+
+        try {
+            usuario = (Usuario) auth.getPrincipal();
+        } catch (Exception e) {
+
+        }
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("usuario", usuario);
         return modelAndView;
     }
 }
