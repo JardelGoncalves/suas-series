06ab988a47efd6e3095c78da32914c672ca7afd2
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-01-18 00:21:37 -0300

    configurando entidade para usar spring security

--- src/main/java/com/jardel/models/Usuario.java
+++ src/main/java/com/jardel/models/Usuario.java
@@ -1,6 +1,6 @@
 package com.jardel.models;
 
-import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
@@ -10,8 +10,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
-public class Usuario implements Serializable {
+public class Usuario implements UserDetails {
+
 
     private static final long serialVersionUID = 1L;
 
@@ -22,7 +26,7 @@ public class Usuario implements Serializable {
     private String nome;
     private String sobrenome;
     private String email;
-    private String password;
+    private String senha;
 
     @OneToMany(mappedBy="usuario")
     private List<Serie> series;
@@ -60,22 +64,21 @@ public class Usuario implements Serializable {
         this.email = email;
     }
 
-    public String getPassword() {
-        return this.password;
+    public String getSenha() {
+        return this.senha;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
     public List<Serie> getSeries() {
-        return series;
+        return this.series;
     }
 
     public void setSeries(List<Serie> series) {
         this.series = series;
     }
-
 
     @Override
     public boolean equals(Object o) {
@@ -92,6 +95,42 @@ public class Usuario implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+    // Metodos gerados do UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+		return true;
+	}
 
 
 }
