97d114047e950ed87966e22ae79a2649fc3e181a
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-01-18 00:25:15 -0300

    implementado autenticação via db

---
+++ src/main/java/com/jardel/security/WebConfigSecurity.java
@@ -0,0 +1,48 @@
+package com.jardel.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthUserDetailService authUserDetailService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/").permitAll() // permite acesso ao / para qualquer usuario
+        .antMatchers(HttpMethod.GET, "/register").permitAll() // permite acesso ao form de cadastro
+        .antMatchers(HttpMethod.POST, "/cadastro-usuario").permitAll() // permite qualquer user se cadastrar
+        .anyRequest().authenticated()
+        .and().formLogin()
+        .defaultSuccessUrl("/dashboard")
+        .and().logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .logoutSuccessUrl("/");
+
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(authUserDetailService)
+        .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**");
+        web.ignoring().antMatchers("/js/**");
+        web.ignoring().antMatchers("/img/**");
+    }
+}
