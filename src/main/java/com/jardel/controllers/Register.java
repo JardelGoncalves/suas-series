e13cedd1ef74a54172b8e2b6e91d6203a53e22ae
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-01-18 00:33:27 -0300

    alterando password por senha no register.html e desautenticando usuario no /register do controlador

--- src/main/java/com/jardel/controllers/Register.java
+++ src/main/java/com/jardel/controllers/Register.java
@@ -4,6 +4,8 @@ import com.jardel.models.Usuario;
 import com.jardel.repository.UsuarioRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +19,10 @@ public class Register{
 
     @GetMapping("/register")
     public ModelAndView registerPage(){
+        // desautenticando o usuario
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        auth.setAuthenticated(false);
+        
         ModelAndView modelAndView = new ModelAndView("register");
         return modelAndView;
     }
